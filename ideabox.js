7e6748ac65616d6b8f42bbd21aedbedfc34d3e26
Jeff Bender <j25bender@gmail.com>
2017-09-04 21:16:55 -0600

    css for new idea cards

--- ideabox.js
+++ ideabox.js
@@ -11,12 +11,12 @@ function prependCard(event) {
 	
 	$('.ideas').prepend(
 		`<div class="ideaBox">
-			<h2>${titleInput.val()}</h2>
+			<h2 class="title-input">${titleInput.val()}</h2>
 				<button class="delete">x</button>
-				<p>${ideaInput.val()}</p>
-				<p class="quality">swill</p>
+				<p class="idea-input">${ideaInput.val()}</p>
 				<button class="up">up</button>
 				<button class="down">down</button>
+				<p class="quality">quality:<span class="quality-type">swill</span></p><hr color="#D1D3D4"/>
 		</div>`
 	);
 	clearInputFields();
@@ -38,37 +38,37 @@ $('.ideas').on('click',function(event){
 
 
 
-$('.ideas').on('click', upIdeaQuality);
+$('.up').on('click', upIdeaQuality);
 
 function upIdeaQuality(event){
 	event.preventDefault();
-	var n = $('.quality').text();
+	var n = $('.quality-type').text();
 	if(event.target.className === 'up'){
 		if(n === 'swill'){
-			$('.quality').text('plausible');
+			$('.quality-type').text('  '+'plausible');
 		}
 	}
 
 	if(event.target.className === 'up'){
 		if(n === 'plausible'){
-			$('.quality').text('genius');
+			$('.quality-type').text('  '+'genius');
 		}
 	}
 }
 
-$('.ideas').on('click', downIdeaQuality);
+$('.down').on('click', downIdeaQuality);
 
 function downIdeaQuality(event){
 	event.preventDefault();
-	var n = $('.quality').text();
+	var n = $('.quality-type').text();
 	if(event.target.className === 'down'){
 		if(n === 'genius'){
-			$('.quality').text('plausible');
+			$('.quality-type').text('  '+'plausible');
 		}
 	}
 	if(event.target.className === 'down'){
 		if(n === 'plausible'){
-			$('.quality').text('swill');
+			$('.quality-type').text('  '+'swill');
 		}
 	}
 }
